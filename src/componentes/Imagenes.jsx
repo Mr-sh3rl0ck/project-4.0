6955b5ff35b201bd3cd14329a7abc404a8c6228a
Mau <mau424284@gmail.com>
2024-09-19 20:18:47 -0600

    Añadi tailwind y configure. Limpie y ordene app.jsx. Añadi componentes "BtnOvaladoX (son pequeños)", "Imgenes.jsx esta en desarrollo". En pages añadi Oportunidades, mi area.

---
+++ src/componentes/Imagenes.jsx
@@ -0,0 +1,18 @@
+
+export function Imagenes() {
+  const data = [
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1499696010180-025ef6e1a8f9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    },
+    {
+      imageLink:
+        "https://images.unsplash.com/photo-1432462770865-65b70566d673?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80",
+    },
+  ];
+
+
+
+
+  
+}
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Asegúrate de que Tailwind está mirando estos archivos.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
