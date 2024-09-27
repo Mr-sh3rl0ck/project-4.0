d7f0ed4398c7e1ea078abcbe6658422ec4883ae6
Mau <mau424284@gmail.com>
2024-09-27 01:47:58 -0600

    app.jsx Actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import { Oportunidades } from './componentes/sections/Oportunidades';
-import { Noticias } from './componentes/sections/Noticias';
+import { Outlet } from 'react-router-dom';
 
 import './index.css'; 
-import './App.css'
+import './App.css';
+import { Oportunidades } from './componentes/sections/Oportunidades';
+import { Noticias } from './componentes/sections/Noticias';
 import SeccionModelo from "./componentes/sections/SeccionDos";
 import NavBar from "./componentes/NavBar";
 
 function App() {
   return (
     <>
-    {/*NavBar*/}
-    <NavBar/>
-    <Outlet/>  
-    <div className="App">
-    {/* <1inicio/> */}
-    <SeccionModelo/>
-    <div className="bg-[#D6BD98] py-96"></div>
-    {/* <Oferta Educativa/> */}
-    <Oportunidades/>
-    <div className="bg-gray-800 py-20"></div>
-    <Noticias/>
-    {/* <Footer/> */}
-    </div>
+      {/* NavBar */}
+      <NavBar />
+      <div className="App">
+        {/* Renderiza las rutas hijas aquí */}
+        <Outlet />
+
+        {/* Contenido adicional */}
+        <SeccionModelo />
+        <div className="bg-[#D6BD98] py-96"></div>
+        
+        {/* Oferta Educativa */}
+        <Oportunidades />
+        <div className="bg-gray-800 py-20"></div>
+        
+        {/* Noticias */}
+        <Noticias />
+        {/* <Footer/> */}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
